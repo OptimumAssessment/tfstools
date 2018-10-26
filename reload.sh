feec69ca5e08a61eddb8c4f641e138a706018240
Rinke van den Berg <rinke.van.den.berg@citrus.nl>
2018-10-26 11:44:13 +0200

    remove personal development help

--- reload.sh
+++
@@ -1,6 +0,0 @@
-
-tmux send-keys -t 1.1 c-c
-tmux send-keys -t 1.1 "tfstools pipeline cafe-bla" ENTER
-# tmux send-keys -t 3.1 "rbe" ENTER
-# sleep 0.2
-# tmux send-keys -t 3.1 "acam32djlc7xqgm3a2zeyllyqh56dbekcqxqukjofon56doebf2q" ENTER
