7910f76fd4ec79b91db24ef6d39f40429017b9cf
Rinke van den Berg <rinke.van.den.berg@citrus.nl>
2018-10-30 16:53:42 +0100

    improve error handling

--- index.js
+++ index.js
@@ -7,31 +7,27 @@ const createPipeline = require('./src/create-pipeline');
 
 
 (async() => {
-    const commandList = Object.keys(map).join(', ');
-    if(process.argv.length < 3) {
-        console.error('Invalid usage. Correct usage: tfstools [command]');
-        console.error('Use one of: '+commandList);
-        process.exit(1);
-    }
-    const [ , , command, ...rest ] = process.argv;
     const map = {
         'repo': createRepo,
         'policy': updateRepoPolicy,
         'pipeline': createPipeline,
     }
+    const commandList = Object.keys(map).join(', ');
+    if(process.argv.length < 3) {
+        throw Error('Invalid usage. Correct usage: tfstools [command]'+"\n"+'Use one of: '+commandList);
+    }
+    const [ , , command, ...rest ] = process.argv;
     const func = map[command];
     if(!func) {
-        console.error('Invalid command '+command+'. Use one of: '+commandList);
-        process.exit(1);
+        throw Error('Invalid command '+command+'. Use one of: '+commandList);
     }
-    func.apply(this,rest).catch( (e) => {
-        console.log(e);
-        if(e.response) {
-            console.log(e.response.data);
-        }
-        process.exit(1);
-    });
-})();
-
+    return func.apply(this,rest);
+})().catch( (e) => {
+    console.log(e);
+    if(e.response) {
+        console.log(e.response.data);
+    }
+    process.exit(1);
+});
 
 
