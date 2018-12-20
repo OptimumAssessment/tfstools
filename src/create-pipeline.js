249704cbeb1a51c760366cae96fcda035775884f
Rinke van den Berg <rinke.van.den.berg@citrus.nl>
2018-12-20 08:59:06 +0100

    show existing pipelines to choose from

--- src/create-pipeline.js
+++ src/create-pipeline.js
@@ -1,27 +1,34 @@
 const getRepo = async(axios, name) => (await axios.get('/_apis/git/repositories/'+name)).data;
 const getRepoName = require('./get-repo-name');
+const promptly = require('promptly');
 
 module.exports = async( name ) => {
     name = await getRepoName(name);
     const axios = await require('./login')();
-    let result;
-
-    const fromName = 'AssessmentPlayer';
-    const fromRepo = await getRepo(axios, fromName);
     const targetRepo = await getRepo(axios, name);
 
+    let result;
     result = await axios.get('/_apis/build/definitions', {
         params: {
-            repositoryType: 'TfsGit',
-            repositoryId: fromRepo.id,
             includeAllProperties: true,
         },
     });
 
-    const original = result.data.value[0];
+    let existing = result.data.value.sort( (a,b) => {
+        return a.name > b.name ? -1 : 1;
+    });
+
+    console.log(existing.map(pl => pl.id + ' ' + pl.name).join("\n"));
+    let id;
+    try {
+        id = await promptly.prompt('Which build pipeline id to clone from?');
+    } catch(e) {
+        return;
+    }
+
+    const original = existing.find(pl => ''+pl.id === id);
     const clone = Object.assign({},original);
     clone.name = targetRepo.name;
-
     Object.assign( clone.repository, {
         "id": targetRepo.id,
         "name": targetRepo.name,
