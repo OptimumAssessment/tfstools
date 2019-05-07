dcba8f26c46fc221a14a975b34184daaec650914
Rinke van den Berg <rbe@citrusandriessen.nl>
2019-05-07 13:08:51 +0200

    Change policy source

--- src/update-repo-policy.js
+++ src/update-repo-policy.js
@@ -6,7 +6,7 @@ module.exports = async( name ) => {
     const axios = await require('./login')();
     let result;
 
-    const fromRepoName = 'Optimum.Documentation';
+    const fromRepoName = 'Cafe.AssessmentPlayer';
     result = await axios.get(`/_apis/git/repositories/${fromRepoName}`)
 
     const fromRepoId = result.data.id;
